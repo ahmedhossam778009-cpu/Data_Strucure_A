4bdb5947e01f341f5a966b13f0cb8bcb2c83bce2
Ahmed Hossam <lokan10@icloud.com>
2026-09-13 04:19:30 +0300

    Implement MArrayList

---
+++ Data_Struct/Data Struct/Array.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Struct.Data_Struct
+{
+    public class MArrayList
+    {
+        object[] array;
+        int max_size;
+        int length;
+        public int Length { get => length; }
+        public bool IsFull()
+        { 
+        return length == max_size;  
+        }  
+
+        public MArrayList(int size)
+        {
+            max_size = size;
+            length = 0;
+            array = new object[max_size];
+            
+        }
+        public MArrayList()
+        { 
+            max_size = 10;
+            length = 0;
+            array = new object[max_size];
+        }
+
+        public void PrintList()
+        {
+            for(int i =0; i<length;i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public void Add(object value)
+        {
+            if (!IsFull())
+            {
+                array[length] = value;
+                length++;
+            }
+            else
+            {
+                Console.WriteLine("Array is full");
+            }
+        }
+        
+        public void Insert(int Index , object Item)
+        {
+            if(!IsFull())
+            { 
+                if(Index < 0 || Index > length)
+                {
+                    Console.WriteLine("Index is out of range");
+                }
+                else
+                {
+                    for (int i = length; i > Index; i--)
+                    {
+                        array[i] = array[i - 1];
+                        
+                    }
+                    array[Index] = Item;
+                    length++;
+                }
+            }
+
+        }
+        public int IndexOf(object value)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (array[i].Equals(value))
+                {
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+        public object RemoveAt(int Index)
+        {
+         if (length == 0)
+         {
+                Console.WriteLine("Array is empty");
+                return null;
+
+         }
+            if (Index < 0 || Index >= length)
+            {
+                Console.WriteLine("Index is out of range");
+                return null;
+            }
+
+            object removedItem = array[Index];
+
+            for (int i = Index; i < length - 1; i++)
+            {
+                array[i] = array[i + 1];
+
+            }
+            Console.WriteLine("Removed item: " + removedItem);
+            length--;
+            array[length] = null;
+            return removedItem;
+        }
+        public void Remove(object value)
+        {
+            int Index = this.IndexOf(value);
+            if (Index == -1)
+            {
+                Console.WriteLine("Item not found");
+                return;
+            }
+
+            for (int i = Index; i < length - 1; i++)
+            {
+                array[i] = array[i + 1];
+
+            }
+            length--;
+            array[length] = null;
+        }
+        
+        
+    }
+}
---
+++ Data_Struct/Data Struct/Program.cs
@@ -0,0 +1,91 @@
+﻿using Data_Struct.Data_Struct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Struct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create MArrayList
+            MArrayList list = new MArrayList(5);
+
+            // =========================
+            // Add
+            // =========================
+
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            Console.WriteLine("After Add:");
+            list.PrintList();
+
+
+            // =========================
+            // Length
+            // =========================
+
+            Console.WriteLine("\nLength: " + list.Length);
+
+
+            // =========================
+            // IndexOf
+            // =========================
+
+            Console.WriteLine("\nIndex of 20: " + list.IndexOf(20));
+
+
+            // =========================
+            // Insert
+            // =========================
+
+            list.Insert(1, 15);
+
+            Console.WriteLine("\nAfter Insert(1, 15):");
+            list.PrintList();
+
+
+            // =========================
+            // RemoveAt
+            // =========================
+
+            object removedItem = list.RemoveAt(2);
+
+            Console.WriteLine("\nAfter RemoveAt(2):");
+            list.PrintList();
+
+            Console.WriteLine("Removed: " + removedItem);
+
+
+            // =========================
+            // Remove
+            // =========================
+
+            list.Remove(15);
+
+            Console.WriteLine("\nAfter Remove(15):");
+            list.PrintList();
+
+
+            // =========================
+            // IsFull
+            // =========================
+
+            Console.WriteLine("\nIs Full: " + list.IsFull());
+
+
+            // =========================
+            // Final Length
+            // =========================
+
+            Console.WriteLine("Final Length: " + list.Length);
+        }
+    }
+}
+    
+
